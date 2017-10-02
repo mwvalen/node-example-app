a4ce66c0f0089d4d023643dcae97edf98cb4d11d
Michael W. Valentine <mwillvalentine@gmail.com>
2017-10-02 00:02:34 -0400

    User endpoints made.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,17 @@
 var router = require('express').Router();
 
+router.use('/', require('./users'));
+
+router.use(function(err, req, res, next) {  //4 args = error handler
+  if (err.name === 'ValidationError'){
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce(function(errors, key){
+        errors[key] = err.errors[key].message;
+
+        return errors;
+      }, {})
+    });
+  }
+});
+
 module.exports = router;
