40df977bae2a0a66cec5a13e9f9de60f2e206eef
mwvalen <mwillvalentine@gmail.com>
2017-10-08 18:13:54 -0400

    Implement following functionality, exposing tags, queryable article lists, and a default article feed based on who a user is following

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -16,7 +16,7 @@ router.param('username', function(req, res, next, username){
 router.get('/:username', auth.optional, function(req, res, next){
   if(req.payload){
     User.findById(req.payload.id).then(function(user){
-      
+
       if(!user){ return res.json({profile: req.profile.toProfileJSONFor(false)}); }
 
       return res.json({profile: req.profile.toProfileJSONFor(user)});
@@ -25,5 +25,31 @@ router.get('/:username', auth.optional, function(req, res, next){
     return res.json({profile: req.profile.toProfileJSONFor(false)});
   }
 });
+
+router.post('/:username/follow', auth.required, function(req, res, next) {
+  var profileId = req.profile._id;
+
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401); }
+
+    return user.follow(profileId).then(function(){
+      return res.json({profile: req.profile.toProfileJSONFor(user)});
+    });
+  }).catch(next);
+});
+
+router.delete('/:username/follow', auth.required, function(req, res, next){
+  var profileId = req.profile._id;
+
+  User.findById(req.payload.id).then(function(user){
+    if (!user) { return res.sendStatus(401);}
+
+    return user.unfollow(profileId).then(function(){
+      return res.json({profile: req.profile.toProfileJSONFor(user)});
+    });
+  })
+});
+
+
 
 module.exports = router;
